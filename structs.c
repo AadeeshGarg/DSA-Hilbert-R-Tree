904e23d1969863acb15f8564297dcf507b0dd387
AadeeshGarg <aadigarg2003@gmail.com>
2023-04-27 15:00:24 +0530

    naming changes

--- structs.c
+++ structs.c
@@ -5,31 +5,33 @@
 #define DIMENSIONS 2
 
 typedef struct point{
-    int px;
-    int py;
-    int hv;
+    int p_x;
+    int p_y;
+    int hilbert_value;
 }point;
 
 typedef struct rect{
-    int tlx;
-    int tly;
-    int brx;
-    int bry;
-    int hv;
+    int topleft_x;
+    int topleft_y;
+    int bottomright_x;
+    int bottomright_y;
+    int hilbert_value;
 }rect;
 
 typedef struct hilbert_node* NODE;
 struct hilbert_node{
-    bool isleaf;
+    bool is_leaf;
     point* pnt;
-    rect r;
-    int num_membr;
-    int lhv;
+    rect rectangle;
+    int number_of_children;
+    int largest_hilbert_value;
     NODE* children;
 };
 
 
 typedef struct hilbert_r_tree{
     NODE root;
-    int hilbertCurveOrder;
+    int hilbert_curve_order;
 }hilbert_r_tree;
+
+
